a3372e3f61e887d1f40c8be426d4d4b46bb5dab4
Summer Bernotas <81632808+sumbernotas@users.noreply.github.com>
2025-02-11 20:10:14 -0500

    Update travel.js

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -3,7 +3,7 @@ var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
