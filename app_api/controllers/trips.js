0adbee32c32c809841d88ec68929432011d054fc
Summer Bernotas <sumrx3@gmail.com>
2025-03-02 12:25:43 -0500

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -2,20 +2,73 @@ const mongoose = require('mongoose');
 const Trip = require('../models/travlr'); // Register model
 const Model = mongoose.model('trips');
 
+// PUT: /trips/:tripCode - Adds a new Trip
+// Regardless of outcome, response must include HTML status
+code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async (req, res) => {
+    // Uncomment for debugging
+    console.log(req.params);
+    console.log(req.body);
+    const q = await Model
+        .findOneAndUpdate(
+            { 'code': req.params.tripCode },
+            {
+                code: req.body.code,
+                name: req.body.name,
+                length: req.body.length,
+                start: req.body.start,
+                resort: req.body.resort,
+                perPerson: req.body.perPerson,
+                image: req.body.image,
+                description: req.body.description
+            }
+        )
+        .exec();
+    if (!q) { // Database returned no data
+        return res
+            .status(400)
+            .json(err);
+    } else { // Return resulting updated trip
+        return res
+            .status(201)
+            .json(q);
+    }
+    // Uncomment the following line to show results of
+    operation
+    // on the console
+    // console.log(q);
+};
+
+// POST: /trips - Adds a new trip
+// regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async (req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        descriptipmnn: req.body.description
+    });
+}
+
 // GET: /trips - lists all the trips
 // Regardless of outcome, response must include HTML status code
 // and JSON message to the requires. ting client
-const tripsList = async(req, res) => {
+const tripsList = async (req, res) => {
     const q = await Model
         .find({}) // No filter, return all records
         .exec();
 
-        // Uncomment the following line to show results of query
-        // on the console
-        // console.log(q);
+    // Uncomment the following line to show results of query
+    // on the console
+    // console.log(q);
 
-    if(!q)
-    { // Database returned no data
+    if (!q) { // Database returned no data
         return res
             .status(404)
             .json(err);
@@ -29,17 +82,16 @@ const tripsList = async(req, res) => {
 // GET: /trips/:tripCode - lists a single trip
 // Regardless of outcome, response must include HTML status code
 // and JSON message to the requires. ting client
-const tripsFindByCode = async(req, res) => {
+const tripsFindByCode = async (req, res) => {
     const q = await Model
-        .find({'code' : req.params.tripCode }) // return single record
+        .find({ 'code': req.params.tripCode }) // return single record
         .exec();
 
-        // Uncomment the following line to show results of query
-        // on the console
-        // console.log(q);
+    // Uncomment the following line to show results of query
+    // on the console
+    // console.log(q);
 
-    if(!q)
-    { // Database returned no data
+    if (!q) { // Database returned no data
         return res
             .status(404)
             .json(err);
@@ -52,5 +104,7 @@ const tripsFindByCode = async(req, res) => {
 
 module.exports = {
     tripsList,
-    tripsFindByCode
+    tripsFindByCode,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
